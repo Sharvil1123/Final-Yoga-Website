67ba8d5d97a93c7aabf92b38f2cde0792cd5c4ef
Sharvil <sharvilbhalke@gmail.com>
2026-07-01 18:14:36 +0530

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import logoDark from "@/assets/hoy-logo-dark.png.asset.json";
+import logoDark from "../assets2/logo-dark.png";
 import { links } from "./ui-kit";
 
 export function Footer() {
